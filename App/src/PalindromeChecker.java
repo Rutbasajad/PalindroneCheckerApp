00b1c11f6d6111fea1f5ee6083d7febf3f77e850
Rutbasajad <rutbazaz03@gmail.com>
2026-03-02 09:25:59 +0530

    [Rutbasajad] Added UC9PalindromeChecker

--- App/src/PalindromeChecker.java
+++ App/src/PalindromeChecker.java
@@ -1,81 +1,21 @@
 public class PalindromeChecker {
 
-    static class Node {
-        char data;
-        Node next;
-
-        Node(char data) {
-            this.data = data;
-            this.next = null;
-        }
+    public static void main(String[] args) {
+        String input = "madam";
+        boolean result = check(input, 0, input.length() - 1);
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + result);
     }
 
-
-    public static boolean isPalindrome(Node head) {
-        if (head == null || head.next == null) {
+    private static boolean check(String s, int start, int end) {
+        if (start >= end) {
             return true;
         }
 
-        Node slow = head;
-        Node fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
         }
 
-        Node secondHalf = reverse(slow);
-        Node firstHalf = head;
-        Node temp = secondHalf;
-
-        while (temp != null) {
-            if (firstHalf.data != temp.data) {
-                return false;
-            }
-            firstHalf = firstHalf.next;
-            temp = temp.next;
-        }
-
-        return true;
-    }
-
-    private static Node reverse(Node head) {
-        Node prev = null;
-        Node current = head;
-
-        while (current != null) {
-            Node next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
-
-        return prev;
-    }
-
-    public static Node createLinkedList(String input) {
-        Node head = null;
-        Node tail = null;
-
-        for (char c : input.toCharArray()) {
-            Node newNode = new Node(c);
-            if (head == null) {
-                head = newNode;
-                tail = newNode;
-            } else {
-                tail.next = newNode;
-                tail = newNode;
-            }
-        }
-
-        return head;
-    }
-
-    public static void main(String[] args) {
-        String input = "level";
-        Node head = createLinkedList(input);
-        boolean result = isPalindrome(head);
-        System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + result);
+        return check(s, start + 1, end - 1);
     }
 }
