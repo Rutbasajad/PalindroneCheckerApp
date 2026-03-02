8c5afbb96a3625fd20b159a11ca71d08928a98c7
Rutbasajad <rutbazaz03@gmail.com>
2026-03-02 09:51:06 +0530

    [Rutbasajad] Added UC11PalindromeChecker

--- App/src/PalindromeChecker.java
+++ App/src/PalindromeChecker.java
@@ -1,19 +1,44 @@
+import java.util.Scanner;
+
 public class PalindromeChecker {
 
     public static void main(String[] args) {
-        String input = "A man a plan a canal Panama";
-        String normalized = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
-        boolean isPalindrome = true;
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < normalized.length() / 2; i++) {
-            if (normalized.charAt(i) != normalized.charAt(normalized.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
+        System.out.print("Input a string: ");
+        String input = scanner.nextLine();
+
+        PalindromeService service = new PalindromeService();
+        boolean result = service.checkPalindrome(input);
+
+        System.out.println("Is Palindrome: " + result);
+
+        scanner.close();
+    }
+}
+
+class PalindromeService {
+
+    public boolean checkPalindrome(String input) {
+
+        if (input == null) {
+            return false;
         }
 
-        System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome);
+        input = input.replaceAll("\\s+", "").toLowerCase();
+
+        int start = 0;
+        int end = input.length() - 1;
+
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
     }
 }
